b2f60e3bea27e79da4a6962ad52faf3b0595b3f2
dev-cm-svg <email@example.com>
2020-07-19 23:36:14 -0400

    Updates

---
+++ precache-manifest.75a9a3f772371e08c1c938531b7397d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1aae66474d29e1c82805dc975330d63",
+    "url": "/robotFriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "be5b0abcf7a8b5a0b8d9",
+    "url": "/robotFriends/static/css/main.cdad4adc.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robotFriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotFriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5b0abcf7a8b5a0b8d9",
+    "url": "/robotFriends/static/js/main.2d802e8d.chunk.js"
+  },
+  {
+    "revision": "70f4d5dc6da80a554205",
+    "url": "/robotFriends/static/js/runtime-main.e0196c21.js"
+  },
+  {
+    "revision": "a64ed299ab6012fdd5d8b0d5c3e9236e",
+    "url": "/robotFriends/static/media/Viafont.a64ed299.woff"
+  }
+]);
